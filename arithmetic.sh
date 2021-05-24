46e1c9ae2f88222fedfb55f0f4f4312a5113f21c
Wendy Park <piaomeixian119@hotmail.com>
2021-05-25 00:22:31 +0900

    Create arithmetic.sh

---
+++ arithmetic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# case 1
+for (( k = 0; k < 50; ++k )); do
+  a=$(( 2*k + 1 ))
+  echo "$a"
+done
+
+# case 2
+for (( k = 0; k < 50; ++k )); do
+  a=$( expr 2 '*' "$k" + 1 )
+  echo "$a"
+done
