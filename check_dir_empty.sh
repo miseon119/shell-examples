6adb31a00cff07068d653d3a10ba3ada4047cedc
Wendy Park <piaomeixian119@hotmail.com>
2021-12-02 11:22:05 +0900

    Update check_dir_empty.sh

--- check_dir_empty.sh
+++ check_dir_empty.sh
@@ -9,3 +9,11 @@ else
     echo "$DIR is Empty"
 fi
 # rest of the logic
+
+
+#----------------- curent pwd/directory 
+#!/bin/bash
+echo "Script executed from: ${PWD}"
+
+BASEDIR=$(dirname $0)
+echo "Script location: ${BASEDIR}"
