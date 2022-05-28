0c1c2c0a2b792006e597766844b3fabe34dc8b3a
Wendy Park <piaomeixian119@hotmail.com>
2022-05-28 13:39:58 +0900

    Update function-note.sh

--- function/function-note.sh
+++ function/function-note.sh
@@ -17,9 +17,9 @@ my_function
 
 echo "after   : value= ${value}"
 
-#---------------------------------------------
-#  sample 2:  add two values and return sum
-#---------------------------------------------
+#-----------------------------------------------------
+#  sample 2:  add two values and return sum, use echo
+#-----------------------------------------------------
 #!/bin/bash
 
 function my_function() {
@@ -32,3 +32,34 @@ function my_function() {
 result=$(my_function 1 2)
  
 echo "my_function [ ${result} ]"
+
+
+#---------------------------------------------
+#  sample 3:  use return syntex
+#---------------------------------------------
+#!/bin/bash
+
+function my_function() {
+   echo "argv: $1 $2"
+   return 100
+}
+
+# call my_function
+my_function "hi" "there" result
+ret=$?
+ 
+echo "my_function [ ${ret} ]"
+
+#---------------------------------------------
+#  sample 4:  use global variable save result
+#---------------------------------------------
+#!/bin/bash
+
+function my_function() {
+   kkk="hi there"
+}
+
+# call my_function
+my_function
+ 
+echo "my_function [ ${kkk} ]"
