f907273a2e40354a11eef60628c0defb27c09a67
Wendy Park <piaomeixian119@hotmail.com>
2022-06-04 15:30:35 +0900

    Create string-note.sh

---
+++ basic/string-note.sh
@@ -0,0 +1,9 @@
+#!usr/bin/env bash
+
+#---------------------------------
+# Get string length
+#---------------------------------
+str="this is a string"
+n=${#str}
+echo "Length of the string is : $n "
+
